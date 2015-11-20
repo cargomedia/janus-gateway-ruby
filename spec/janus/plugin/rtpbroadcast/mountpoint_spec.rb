a3c52a9b6d437f045ea049a26f5865501673ba23
Chris Stasiak <me@krzysztof-stasiak.com>
2015-11-20 09:45:15 +0000

    Spec skeleton

---
+++ spec/janus/plugin/rtpbroadcast/mountpoint_spec.rb
@@ -0,0 +1,30 @@
+require 'janus'
+
+describe Janus::Plugin::Rtpbroadcast::Mountpoint do
+
+  let(:client) { Janus::Client.new('ws://10.10.10.111:8188/janus') }
+  let(:session) { Janus::Session.new(client) }
+  let(:plugin) { Janus::Plugin.new(session, Janus::Plugin::Rtpbroadcast.plugin_name) }
+  let(:rtp_mountpoint) { Janus::Plugin::Rtpbroadcast::Mountpoint.new(plugin, 'test-mountpoint') }
+
+  it 'should create rtpbroadcast mountpoint' do
+
+    _self = self
+
+    client.on :open do |data|
+      _self.session.on :create do |id|
+        _self.plugin.on :create do |id|
+          _self.rtp_mountpoint.on :create do |data|
+            _self.client.destroy
+          end
+          _self.rtp_mountpoint.create
+        end
+        _self.plugin.attach
+      end
+      _self.session.create
+    end
+
+    client.connect
+
+  end
+end
