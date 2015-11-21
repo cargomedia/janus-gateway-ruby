46a63f91db069031776f461134c50c27ad8deccd
Chris Stasiak <me@krzysztof-stasiak.com>
2015-11-21 13:16:59 +0000

    Updated resource required methods

--- lib/janus/resource.rb
+++ lib/janus/resource.rb
@@ -6,11 +6,15 @@ module Janus
       raise("`#{__method__}` is not implemented for `#{self.class.name}`")
     end
 
-    def on_created
+    def create
       raise("`#{__method__}` is not implemented for `#{self.class.name}`")
     end
 
     def destroy
+      raise("`#{__method__}` is not implemented for `#{self.class.name}`")
+    end
+
+    def on_created
       raise("`#{__method__}` is not implemented for `#{self.class.name}`")
     end
   end
