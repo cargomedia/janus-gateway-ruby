7ff0ade66c148cb4b2f0da834f94696d74c5b22d
Chris Stasiak <me@krzysztof-stasiak.com>
2015-11-21 12:42:09 +0000

    Added spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'janus'
+require 'eventmachine'
+require 'event_emitter'
+
+require File.expand_path('../websocket_client_mock', __FILE__)
+require File.expand_path('../event_mock', __FILE__)
