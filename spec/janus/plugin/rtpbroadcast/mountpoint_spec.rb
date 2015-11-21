4c22e0fdeafffa9dcd512c9b9a5f7e3b9733cb69
Chris Stasiak <me@krzysztof-stasiak.com>
2015-11-21 12:42:36 +0000

    Adjusted mountpoint spec to work with spec helper

--- spec/janus/plugin/rtpbroadcast/mountpoint_spec.rb
+++ spec/janus/plugin/rtpbroadcast/mountpoint_spec.rb
@@ -1,65 +1,4 @@
-require 'janus'
-require 'eventmachine'
-require 'event_emitter'
-
-class EventMock
-
-  def initialize(data = nil)
-    @data = data || '{}'
-  end
-
-  def data
-    @data
-  end
-
-end
-
-class WebSocketClientMock
-
-  include EventEmitter
-
-  def initialize(*args)
-    connect_mock
-  end
-
-  def connect_mock
-    Thread.new do
-      sleep(0.5)
-      self.emit :open, EventMock.new
-    end
-  end
-
-  def send(data)
-    data_json = JSON.parse(data)
-    response = '{}'
-
-    case data_json['janus']
-      when 'create'
-        response = '{"janus":"success", "transaction":"ABCDEFGHIJK", "data":{"id":"12345"}}'
-      when 'attach'
-        response = '{"janus":"success", "session_id":12345, "transaction":"ABCDEFGHIJK", "data":{"id":"54321"}}'
-      when 'message'
-        response = [
-          '{"janus":"success", "session_id":12345, "sender_id":"54321", "transaction":"ABCDEFGHIJK"',
-          '"plugindata":{"plugin":"janus.plugin.cm.rtpbroadcast", "data":{"streaming":"created"',
-          '"created":"rndmvr-studio-agent-bulldog1448032631000", "stream":{"id":"rndmvr-studio-agent-bulldog1448032631000"',
-          '"description":"rndmvr-studio-agent-bulldog1448032631000", "streams":[{"audioport":8576, "videoport":8369}]}}}}'
-        ].join(',')
-    end
-
-    Thread.new do
-      sleep(0.5)
-      self.emit :message, EventMock.new(response)
-    end
-  end
-
-  def close
-    Thread.new do
-      sleep(0.1)
-      self.emit :close, EventMock.new
-    end
-  end
-end
+require "spec_helper"
 
 describe Janus::Plugin::Rtpbroadcast::Resource::Mountpoint do
 
@@ -70,7 +9,18 @@ describe Janus::Plugin::Rtpbroadcast::Resource::Mountpoint do
 
   it 'should create rtpbroadcast mountpoint' do
 
-    client.stub(:websocket_client_new).and_return(WebSocketClientMock.new)
+    janus_response = {
+      :create => '{"janus":"success", "transaction":"ABCDEFGHIJK", "data":{"id":"12345"}}',
+      :attach => '{"janus":"success", "session_id":12345, "transaction":"ABCDEFGHIJK", "data":{"id":"54321"}}',
+      :message => [
+        '{"janus":"success", "session_id":12345, "sender_id":"54321", "transaction":"ABCDEFGHIJK"',
+        '"plugindata":{"plugin":"janus.plugin.cm.rtpbroadcast", "data":{"streaming":"created"',
+        '"created":"rndmvr-studio-agent-bulldog1448032631000", "stream":{"id":"rndmvr-studio-agent-bulldog1448032631000"',
+        '"description":"rndmvr-studio-agent-bulldog1448032631000", "streams":[{"audioport":8576, "videoport":8369}]}}}}'
+      ].join(',')
+    }
+
+    client.stub(:websocket_client_new).and_return(WebSocketClientMock.new(janus_response))
     client.stub(:transaction_id_new).and_return('ABCDEFGHIJK')
 
     _self = self
