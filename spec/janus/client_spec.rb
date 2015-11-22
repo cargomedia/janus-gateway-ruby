0aba3d3925042ea4f20c3e1a874fb5237fbf86fc
Chris Stasiak <me@krzysztof-stasiak.com>
2015-11-22 12:17:18 +0000

    Added spec for client

---
+++ spec/janus/client_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+describe Janus::Resource::Session do
+  let(:client) { Janus::Client.new('') }
+
+  it 'should timeout transaction' do
+
+    janus_response = {
+      :timeout => '{"janus":"success", "transaction":"000"}'
+    }
+
+    client.stub(:websocket_client_new).and_return(WebSocketClientMock.new(janus_response))
+    client.stub(:transaction_id_new).and_return('000')
+    client.stub(:_promise_wait_timeout).and_return(0.001)
+
+    _self = self
+    client.on :open do
+      _self.client.send_transaction({:janus => "timeout"}).rescue do |error|
+        _self.client.destroy
+      end
+    end
+
+    client.connect
+
+  end
+end
