e58eb6ecc628068bcc207fb6432fdb82c4d4930b
Chris Stasiak <me@krzysztof-stasiak.com>
2015-11-22 09:46:02 +0000

    Added spec for mountpoint error

--- spec/janus/plugin/rtpbroadcast/mountpoint_spec.rb
+++ spec/janus/plugin/rtpbroadcast/mountpoint_spec.rb
@@ -36,4 +36,34 @@ describe Janus::Plugin::Rtpbroadcast::Resource::Mountpoint do
 
     client.connect
   end
+
+  it 'should handle error for mountpoint create' do
+
+    janus_response = {
+      :create => '{"janus":"success", "transaction":"ABCDEFGHIJK", "data":{"id":"12345"}}',
+      :attach => '{"janus":"success", "session_id":12345, "transaction":"ABCDEFGHIJK", "data":{"id":"54321"}}',
+      :message => [
+        '{"janus":"success", "session_id":12345, "sender_id":"54321", "transaction":"ABCDEFGHIJK"',
+        '"plugindata":{"plugin":"janus.plugin.cm.rtpbroadcast", "data":{"error_code":456, "error": "Cannot create mounpoint"}}}'
+      ].join(',')
+    }
+
+    client.stub(:websocket_client_new).and_return(WebSocketClientMock.new(janus_response))
+    client.stub(:transaction_id_new).and_return('ABCDEFGHIJK')
+    rtp_mountpoint.stub(:name).and_return('')
+
+    _self = self
+    client.on :open do
+      _self.session.create.then do
+        _self.plugin.create.then do
+          _self.rtp_mountpoint.create.catch do
+            _self.client.destroy
+          end
+        end
+      end
+    end
+
+    client.connect
+  end
+
 end
