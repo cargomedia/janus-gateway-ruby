dc0fc6fa0e30034b6289adce2488869aa8cf5252
Chris Stasiak <me@krzysztof-stasiak.com>
2015-11-22 09:45:30 +0000

    Added janus standard error class

---
+++ lib/janus/error.rb
@@ -0,0 +1,14 @@
+module Janus
+
+  class Error < StandardError
+
+    def initialize(error_code, error_info)
+      @code, @info = error_code, error_info
+    end
+
+    def message
+      "<Code: #{@code}> <Info: #{@info}>"
+    end
+
+  end
+end
