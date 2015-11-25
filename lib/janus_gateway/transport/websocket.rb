998d1d3c47c77197fba381c8a10ae64d26d5576c
Chris Stasiak <me@krzysztof-stasiak.com>
2015-11-25 09:03:42 +0100

    Fail if client for websocket transport is not existing

--- lib/janus_gateway/transport/websocket.rb
+++ lib/janus_gateway/transport/websocket.rb
@@ -99,7 +99,7 @@ module JanusGateway
 
     # @return [Faye::WebSocket::Client]
     def client
-      @client
+      @client || raise('Client for WebSocket transport is not created!')
     end
 
     private
