cee2929bfcc7009173b86d6082ebddf999bddf16
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-18 13:10:15 +0100

    Removed http janus class; replaced with _send()

--- lib/janus_gateway/transport/http.rb
+++ lib/janus_gateway/transport/http.rb
@@ -11,24 +11,19 @@ module JanusGateway
     # @param [Hash] data
     def send(data)
       Thread.new do
-        uri = URI.parse(@url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
-        request.body = JSON.generate(data)
-        response = http.request(request)
-
-        data = JSON.parse(response.body)
+        response = _send(JSON.generate(data))
 
         transaction_list = @transaction_queue.clone
 
-        transaction_id = data['transaction']
+        transaction_id = response['transaction']
+
         unless transaction_id.nil?
           promise = transaction_list[transaction_id]
           unless promise.nil?
-            if %w(success ack).include?(data['janus'])
-              promise.set(data).execute
+            if %w(success ack).include?(response['janus'])
+              promise.set(response).execute
             else
-              error_data = data['error']
+              error_data = response['error']
               error = JanusGateway::Error.new(error_data['code'], error_data['reason'])
               promise.fail(error).execute
             end
@@ -66,10 +61,17 @@ module JanusGateway
       promise
     end
 
-    # @return [JanusHTTPClient]
-    attr_reader :client
-
     private
+
+    def _send(data)
+      uri = URI.parse(@url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+      request.body = JSON.generate(data)
+      response = http.request(request)
+
+      JSON.parse(response.body)
+    end
 
     # @return [Float, Integer]
     def _transaction_timeout
