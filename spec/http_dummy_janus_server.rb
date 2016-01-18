d335d58c729a737200822c931b9ce92f211e76cd
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-18 13:08:26 +0100

    Added dummy janus server

---
+++ spec/http_dummy_janus_server.rb
@@ -0,0 +1,13 @@
+class HttpDummyJanusServer
+  def initialize(response)
+    @response = response
+  end
+
+  def respond(data)
+    data_json = JSON.parse(data)
+    janus_action_name = data_json['janus'].to_sym
+
+    sleep(0.1)
+    JSON.parse(@response[janus_action_name])
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,4 @@ require 'eventmachine'
 require 'events'
 
 require File.expand_path('../websocket_client_mock', __FILE__)
+require File.expand_path('../http_dummy_janus_server', __FILE__)
