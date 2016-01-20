211b8a2997885757a5ff56baac4ab15bdcaec98a
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-20 10:37:32 +0100

    Updated dummy janus server

--- spec/http_dummy_janus_server.rb
+++ spec/http_dummy_janus_server.rb
@@ -3,10 +3,10 @@ class HttpDummyJanusServer
     @response = response
   end
 
+  # @param [Hash] data
   def respond(data)
     janus_action_name = data[:janus].to_sym
-
-    sleep(0.1)
-    JSON.parse(@response[janus_action_name])
+    response = JSON.parse(@response[janus_action_name])
+    Concurrent::Promise.fulfill(response)
   end
 end
