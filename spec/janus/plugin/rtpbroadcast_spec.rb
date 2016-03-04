7fa5fc49cade6413afcd2bbf27f002be1cd40165
Chris Stasiak <me@krzysztof-stasiak.com>
2016-03-04 09:56:42 +0100

    Added spec

--- spec/janus/plugin/rtpbroadcast_spec.rb
+++ spec/janus/plugin/rtpbroadcast_spec.rb
@@ -34,4 +34,32 @@ describe JanusGateway::Plugin::Rtpbroadcast do
 
     client.run
   end
+
+  it 'should watch mountpoint and relay as udp' do
+    janus_response = {
+      create: '{"janus":"success", "transaction":"ABCDEFGHIJK", "data":{"id":"12345"}}',
+      attach: '{"janus":"success", "session_id":12345, "transaction":"ABCDEFGHIJK", "data":{"id":"54321"}}',
+      message: [
+        '{"janus":"success", "session_id":12345, "sender_id":"54321", "transaction":"ABCDEFGHIJK"',
+        '"plugindata":{"plugin":"janus.plugin.cm.rtpbroadcast", "data":{"streaming":"preparing"}}}'
+      ].join(',')
+    }
+
+    transport.stub(:_create_client).and_return(WebSocketClientMock.new(janus_response))
+    transport.stub(:transaction_id_new).and_return('ABCDEFGHIJK')
+
+    expect(session).to receive(:create).once.and_call_original
+    expect(plugin).to receive(:watch_udp).once.and_call_original
+    expect(EventMachine).to receive(:stop).once.and_call_original
+
+    client.on :open do
+      session.create.then do
+        plugin.watch_udp('mountpoint-1', []).then do
+          EventMachine.stop
+        end
+      end
+    end
+
+    client.run
+  end
 end
